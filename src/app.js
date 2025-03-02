d65e8ca705dbc1b1eefb9d0b7a8b5ac170c42dd8
Alex Wauquier <alex@alexwauquier.com>
2025-03-02 22:35:26 +0100

    feat(api): add products route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import productsRouter from './routes/products.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Smart Hotel RESTful API');
 });
+
+app.use('/api/products', productsRouter);
 
 export default app;
