253ed16ca06d6442e53d5bd1471d1083c5adc832
Alex Wauquier <alex@alexwauquier.com>
2025-03-03 21:34:29 +0100

    feat(products): add routes to manage products

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,23 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Products route');
+    res.send('Get all products');
+});
+
+router.get('/:id', (req, res) => {
+    res.send(`Get product ${req.params.id}`);
+});
+
+router.post('/', (req, res) => {
+    res.send('Create a product');
+});
+
+router.put('/:id', (req, res) => {
+    res.send(`Update product ${req.params.id}`);
+});
+
+router.delete('/:id', (req, res) => {
+    res.send(`Delete product  ${req.params.id}`);
 });
 
 export default router;
