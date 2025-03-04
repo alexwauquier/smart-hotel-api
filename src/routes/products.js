2c07943405e82f89f6be83695088cdcc3e4a3013
Alex Wauquier <alex@alexwauquier.com>
2025-03-04 19:37:57 +0100

    style(products): fix indentation

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,21 +4,21 @@ import * as productsController from '../controllers/products.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Get all products');
+  res.send('Get all products');
 });
 
 router.get('/:id', productsController.getProduct);
 
 router.post('/', (req, res) => {
-    res.send('Create a product');
+  res.send('Create a product');
 });
 
 router.put('/:id', (req, res) => {
-    res.send(`Update product ${req.params.id}`);
+  res.send(`Update product ${req.params.id}`);
 });
 
 router.delete('/:id', (req, res) => {
-    res.send(`Delete product  ${req.params.id}`);
+  res.send(`Delete product  ${req.params.id}`);
 });
 
 export default router;
